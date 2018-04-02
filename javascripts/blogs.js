7ba1004fa33ebf14a04c7710a7ae6c79e74f70b6
Amanda Slayton <alslayton86@gmail.com>
2018-04-02 12:51:17 -0500

    Update blogs.js

--- javascripts/blogs.js
+++ javascripts/blogs.js
@@ -4,6 +4,7 @@ const createBlogPost = (anArray) => {
   let createAString = "";
   for(var i = 0; i < anArray.length; i++){
     createAString += `<article class='blog-post'>`;
+    createAString += `<img src='thumbtack.png' alt='thumbtack' class='thumbtack'>`;
     createAString += `<h3 class='blog-date'>${anArray[i].date}<h3>`;
     createAString += `<h5 class='blog-title'>${anArray[i].title}<h5>`;
     createAString += `<p class='blog-body'>${anArray[i].post}</p>`;
