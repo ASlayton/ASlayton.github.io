ae2a1b9b307074537f83eae0b6e5f45f80fdb5e0
Amanda Slayton <alslayton86@gmail.com>
2018-04-15 15:58:15 -0500

    connected grunt stuff

--- javascripts/blogs.js
+++ javascripts/blogs.js
@@ -1,8 +1,10 @@
+const writeToDom = require('./writeToDom');
+
 // BLOG JAVASCRIPT
 
 const createBlogPost = (anArray) => {
-  let createAString = "";
-  for(var i = 0; i < anArray.length; i++){
+  let createAString = '';
+  for (let i = 0; i < anArray.length; i++) {
     createAString += `<article class='blog-post'>`;
     createAString += `<img src='/images/thumbtack.png' alt='thumbtack' class='thumbtack'>`;
     createAString += `<h3 class='blog-date'>${anArray[i].date}<h3>`;
@@ -13,20 +15,20 @@ const createBlogPost = (anArray) => {
   return createAString;
 };
 
-function ifItFails(){
-  console.log("Mistakes have been made.")
+function ifItFails () {
+  console.log('Mistakes have been made.');
 };
 
-function ifItLoads(){
+function ifItLoads () {
   const blogData = JSON.parse(this.responseText);
-  writeToDom(createBlogPost(blogData.blogs),"blog-post-container");
+  writeToDom(createBlogPost(blogData.blogs),'blog-post-container');
 };
 
-function startApplication(){
-  let myRequest = new XMLHttpRequest();
-  myRequest.addEventListener("load", ifItLoads);
-  myRequest.addEventListener("error", ifItFails);
-  myRequest.open("GET", "../db/blog.json");
+const startApplication = () => {
+  const myRequest = new XMLHttpRequest();
+  myRequest.addEventListener('load', ifItLoads);
+  myRequest.addEventListener('error', ifItFails);
+  myRequest.open('GET', '../db/blog.json');
   myRequest.send();
 };
 
