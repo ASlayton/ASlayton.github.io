d21d13372f139ae57b507e16ce2f0fddaed80471
Amanda Slayton <alslayton86@gmail.com>
2018-05-30 17:27:56 -0500

    fixed some styling errors- header/footer and font

--- javascripts/projects.js
+++ javascripts/projects.js
@@ -6,7 +6,7 @@ const buildProjects = () => {
   projectData.forEach((project) => {
     domString += `<div class="panel panel-default project-container col-md-4">`;
     domString +=   `<div class="panel-heading">`;
-    domString +=     `<h3 class="panel-title">${project.title}</h3>`;
+    domString +=     `<h1 class="panel-title">${project.title}</h1>`;
     domString +=   `</div>`;
     domString +=   `<div class="panel-body">`;
     domString +=     `<img src="${project.imageURL}" alt="${project.altText}" class="project-img">`;
