4a34a6981970e06fcf86065d7a309c0612b5c9a9
Amanda Slayton <alslayton86@gmail.com>
2018-06-29 22:22:02 -0500

    minor styling changes

--- javascripts/blog.js
+++ javascripts/blog.js
@@ -3,7 +3,7 @@ const writeToDom = require('./writeToDom');
 const buildBlog = (blogData) => {
   let domString = '';
   blogData.forEach((post) => {
-    domString += `<div class='blog-post-container'>`;
+    domString += `<div class='blog-post-container' id="${post.id}">`;
     domString += `<h3 col-md-6>${post.title}</h3>`;
     domString += `<h4 col-md-6 text-right>${post.date}</h4>`;
     domString += `<p>${post.post}</p>`;
@@ -12,6 +12,16 @@ const buildBlog = (blogData) => {
   writeToDom.writeToDom(domString, '#blog-content');
 };
 
+const buildArchive = (blogData) => {
+  let domString = '<img src="/images/clipboardimg.png"><ul><h3>Archive:</h3>';
+  blogData.forEach((post) => {
+    domString += `<li><a title="${post.title}" href="#${post.id}">${post.date}</a></li>`;
+  });
+  domString += '</ul>';
+  writeToDom.writeToDom(domString, '#blog-archive');
+};
+
 module.exports = {
   buildBlog,
+  buildArchive,
 };
